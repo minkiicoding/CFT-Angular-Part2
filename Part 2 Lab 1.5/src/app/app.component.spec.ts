3fa145fc505fbb8e0bd3db7821a964655ae73383
minkiicoding <panita.sy@gmail.com>
2021-04-03 20:46:44 +0700

    Part2

--- Part 2 Lab 1.5/src/app/app.component.spec.ts
+++ Part 2 Lab 1.5/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-project'`, () => {
+  it(`should have as title 'Angular part2 Lab 1.5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-project');
+    expect(app.title).toEqual('Angular part2 Lab 1.5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-first-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular part2 Lab 1.5 app is running!');
   });
 });
--- Part 2 Lab 1.5/src/app/app.module.ts
+++ Part 2 Lab 1.5/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    Home2Component,
 
   ],
   imports: [
